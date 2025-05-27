f27cf9e9a32c837b2d74a833c8880293dfc9b8ce
joel-gbsn <joel_gibson_93@hotmail.com>
2025-05-27 14:36:24 +0100

    new Reader class set up

--- ScaleLibrary/src/datamanagement/Reader.java
+++ ScaleLibrary/src/datamanagement/Reader.java
@@ -1,5 +1,70 @@
 package datamanagement;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import util.Scale;
+import util.Interval;
+
 public class Reader {
+	
+	private String filename;
+	
+	public Reader(String filename) {
+		this.filename = filename;
+	}
+	
+	public List<Scale> readScales() {
+		// the list to store each line
+		List<Scale> scales = new ArrayList<>();
+		
+		// prepare the file for reading
+		File file = new File(filename);
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {	
+			// create readers to read the file
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			
+			// read each line from the file
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				String data[] = line.trim().split(";\\s*");
+				
+				String format = data[0].toLowerCase().trim();
+				String name = data[1].toLowerCase().trim();
+				String[] intervalList = data[2].split(",\\s*");
+				boolean simplify = Boolean.valueOf(data[3].trim());
+				
+				Interval[] intervals = new Interval[intervalList.length];
+				for (int i = 0; i < intervalList.length; i++) {
+					intervals[i] = Interval.getInterval(intervalList[i]);
+				}
+				
+				scales.add(new Scale(name, name, format, intervals));
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				// close the file readers
+				fr.close();
+				br.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return scales;
+	}
 
 }
