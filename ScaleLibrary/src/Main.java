a53ae7a031362e117abd94584a1f8f1e2a3d879e
joel-gbsn <joel_gibson_93@hotmail.com>
2025-06-13 13:15:18 +0100

    Main function set up

--- ScaleLibrary/src/Main.java
+++ ScaleLibrary/src/Main.java
@@ -1,4 +1,14 @@
+import datamanagement.Reader;
+import processor.Processor;
+import ui.UserInterface;
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		String filename = "ScaleLibrary/src/scales.txt";
+		Reader reader = new Reader(filename);
+		Processor processor = new Processor(reader);
+		UserInterface ui = new UserInterface(processor);
+		ui.start();
+	}
 }
