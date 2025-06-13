21eee7c6feda4a17be7730c4506cd011f074b0d6
joel-gbsn <joel_gibson_93@hotmail.com>
2025-06-13 13:38:04 +0100

    Processor set up

--- ScaleLibrary/src/processor/Processor.java
+++ ScaleLibrary/src/processor/Processor.java
@@ -1,5 +1,18 @@
 package processor;
 
+import java.util.List;
+
+import datamanagement.Reader;
+import util.Scale;
+
 public class Processor {
+	
+	private Reader reader;
+	private List<Scale> scales;
+	
+	public Processor(Reader reader) {
+		this.reader = reader;
+		this.scales = reader.readScales();
+	}
 
 }
