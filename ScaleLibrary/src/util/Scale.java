291be18f9aa19a9869548f4ea0b3430402510f2a
joel-gbsn <joel_gibson_93@hotmail.com>
2025-06-18 10:55:55 +0100

    Functionality for calculating scales added

--- ScaleLibrary/src/util/Scale.java
+++ ScaleLibrary/src/util/Scale.java
@@ -1,9 +1,14 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Scale {
 	
 	// structure for memoization
-	// map
+	Map<String, List<Note>> scales = new HashMap<String, List<Note>>();
 	
 	// e.g. melodic minor
 	String name;
@@ -26,4 +31,23 @@ public class Scale {
 		this.simplify = simplify;
 	}
 	
+	public List<Note> getScale(String root) {
+		root = root.substring(0, 1).toUpperCase() + root.substring(1);
+		
+		if (scales.containsKey(root)) {
+			return scales.get(root);
+		}
+		
+		List<Note> newScale = generateScale(root);
+		if (newScale != null) {
+			scales.put(root, newScale);
+		}
+		return newScale;
+	}
+	
+	private List<Note> generateScale(String root) {
+		List<Note> scale = new ArrayList<>();
+		
+		return scale;
+	}
 }
